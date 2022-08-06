ae9b15c4a11f21c54f35d4191dcc6a1ea7a001c2
Patrick <patasax22@yahoo.com>
2022-08-06 14:05:46 -0400

    quiz start and question display logic added

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 
 function startGame() {
     console.log('Started');
@@ -12,24 +15,46 @@ function startGame() {
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
     setNextQuestion();
-}
+};
 
 function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+};
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    })
+};
+
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+};
+
+function selectAnswer(e) {
     
-}
-
-function selectAnswer() {
-
-}
+};
 
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'What is 2 + 2?',
         answers: [
-            { text: '4', correct: true }
-            { text: '5', correct: false }
-            { text: '2', correct: false }
+            { text: '4', correct: true },
+            { text: '5', correct: false },
+            { text: '2', correct: false },
             { text: '8', correct: false }
         ]
     }
-]
+];
